d0b5df39b549a844cdfb2453eb25e484614e51e4
Takumi19980801 <takunomi1998@gmail.com>
2024-01-10 20:29:01 +0900

    ユーザー管理(修正) first commit

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     nickname              { Faker::Name.initials(number: 2) }
     email                 { Faker::Internet.email }
-    password              { Faker::Internet.password(min_length: 6) }
+    password              { '1a' + Faker::Internet.password(min_length: 6) }
     password_confirmation { password }
     birth_day             { Faker::Date.birthday(min_age: 18, max_age: 65).strftime('%Y-%m-%d') }
     first_name            { Faker::Japanese::Name.first_name }
