65233fc6aa42bb20c301d5eca80d35db71a0f2b5
Takumi19980801 <takunomi1998@gmail.com>
2024-01-17 16:07:54 +0900

    商品削除機能実装(修正)

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -39,8 +39,12 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    @item.destroy
-    redirect_to root_path
+    if current_user && @item.user == current_user
+      @item.destroy
+      redirect_to root_path
+    else
+      redirect_to root_path
+    end
   end
 
   private
