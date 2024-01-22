64a127945488c75d03abe1d0b43b7f14c0a1667a
Takumi19980801 <takunomi1998@gmail.com>
2024-01-22 19:30:26 +0900

    商品購入機能(修正) second commit

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -17,10 +17,7 @@ class OrderAddress
   validates :prefecture_id, numericality: {other_than: 1, message: "can't be blank"}
 
   def save
-    # 寄付情報を保存し、変数donationに代入する
     order = Order.create(item_id: item_id, user_id: user_id)
-    # 住所を保存する
-    # donation_idには、変数donationのidと指定する
     Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, street: street, building_name: building_name, phone_number: phone_number, order_id: order.id)
   end
 end
