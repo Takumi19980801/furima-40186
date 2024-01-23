bf267793932fec48fc79fc17c43f4369559a0b78
Takumi19980801 <takunomi1998@gmail.com>
2024-01-23 17:00:12 +0900

    本番環境(修正)

--- app/javascript/price.js
+++ app/javascript/price.js
@@ -5,8 +5,21 @@ window.addEventListener('turbo:load', function(){
     const profitDom = document.getElementById("profit");
     const price = priceInput.value;
     const tax = Math.floor(price * 0.1);
-    const profit = Math.floor(price * 0.9);
-    addTaxDom.innerHTML = tax;
-    profitDom.innerHTML = profit;
+    const profit = Math.ceil(price * 0.9);
+    addTaxDom.textContent = tax;
+    profitDom.textContent = profit;
   });
-})
+});
+
+window.addEventListener("turbo:render", function() {
+  const priceInput = document.getElementById("item-price");
+  priceInput.addEventListener("input", () => {
+    const addTaxDom = document.getElementById("add-tax-price");
+    const profitDom = document.getElementById("profit");
+    const price = priceInput.value;
+    const tax = Math.floor(price * 0.1);
+    const profit = Math.ceil(price * 0.9);
+    addTaxDom.textContent = tax;
+    profitDom.textContent = profit;
+  });
+});
